3cc98129d023ebf9718dbb7a1af2c086402e3358
Amanda Marcos <amandakkm0202@gmail.com>
2024-06-24 09:39:14 -0300

    Cadastro controller

--- FinalJayme/src/main/java/com/br/FinalJayme/controllers/MedicoController.java
+++ FinalJayme/src/main/java/com/br/FinalJayme/controllers/MedicoController.java
@@ -1,9 +1,13 @@
 package com.br.FinalJayme.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.br.FinalJayme.entities.Medico;
 import com.br.FinalJayme.services.MedicoService;
 
 @RestController
@@ -12,5 +16,10 @@ public class MedicoController {
 
     @Autowired
     MedicoService service;
+
+    @PostMapping("/cadastro")
+    public ResponseEntity<String> cadastrar(Medico medico) {
+        return new ResponseEntity<>(service.cadastro(medico), HttpStatus.OK);
+    }
 
 }
--- FinalJayme/src/main/java/com/br/FinalJayme/controllers/ProntuarioController.java
+++ FinalJayme/src/main/java/com/br/FinalJayme/controllers/ProntuarioController.java
@@ -1,9 +1,13 @@
 package com.br.FinalJayme.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.br.FinalJayme.entities.Prontuario;
 import com.br.FinalJayme.services.ProntuarioService;
 
 @RestController
@@ -12,5 +16,10 @@ public class ProntuarioController {
 
     @Autowired
     ProntuarioService service;
+
+    @PostMapping("/cadastro")
+    public ResponseEntity<String> cadastrar(Prontuario prontuario) {
+        return new ResponseEntity<>(service.cadastro(prontuario), HttpStatus.OK);
+    }
 
 }
